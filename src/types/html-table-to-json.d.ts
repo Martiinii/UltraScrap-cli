37a6c0fa1ff5a40e5bd3ed11f708f43b214ba7f3
Martini <me.martiinii@gmail.com>
2023-08-30 20:53:02 +0200

    Removed declaration file

--- src/types/html-table-to-json.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'html-table-to-json' {
-    type ParseResult<T extends ReadOnlyArray<string>, V = string> = {
-        results: { [K in (T extends ReadonlyArray<infer U> ? U : never)]: V }[][]
-        count: number,
-    };
-
-    export function parse<T extends ReadonlyArray<string>>(html: string): ParseResult<T>
-}

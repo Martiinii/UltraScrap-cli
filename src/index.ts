18fb92bd7e1978269add6717a0a4b8d5d682d67a
Martini <me.martiinii@gmail.com>
2023-08-30 20:52:56 +0200

    Testing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { downloadSong } from './songs/invidual/downloadSong.js';
 import { fetchSongPage } from './songs/list/fetchSongs.js';
-import { setLoginCookie } from './utils/login.js';
+import { getLoginCookie, setLoginCookie } from './utils/login.js';
 import promptInit from 'prompt-sync';
 export const prompt = promptInit({ sigint: true })
 
-
 await setLoginCookie();
+
+const page = await fetchSongPage(1);
+
+console.log(page)

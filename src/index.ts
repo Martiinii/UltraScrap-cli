247e2ed72bf677ce25431d6869ea05cbb046b4e9
Martiinii <me.martiinii@gmail.com>
2023-08-30 18:46:09 +0200

    Testing new functionality

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
+import { fetchSongPage } from './songs/list/fetchSongs.js';
 import { setLoginCookie } from './utils/login.js';
-import { downloadSong } from './songs/downloadSong.js';
 import promptInit from 'prompt-sync';
-
-await setLoginCookie();
 export const prompt = promptInit({ sigint: true })
 
-while (true) {
-    console.log(''); // Creates gap between downloaded songs. Do not use \n in prompt, it will cause visual glitches
-    const songId = prompt('Input song id: ');
 
-    await downloadSong(songId);
-}
+await setLoginCookie();
